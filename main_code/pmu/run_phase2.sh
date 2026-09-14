6692ab05644f4cc9038757b1e8f5fb1edd6fd185
Justin Richard Frank Kuethe <jrkuethe@ncsu.edu>
2026-09-14 17:25:11 -0400

    Artemisia associaitivity verification

--- main_code/pmu/run_phase2.sh
+++ main_code/pmu/run_phase2.sh
@@ -56,20 +56,20 @@ python3 stride_correlation.py \
       --out ../../data_raw/thunderbird/pmu/stride_correlation
 
 python3 associativity_correlation.py \
-      --host upgrade --level l1 \
+      --host artemisia --level l1 \
       --plan-json ../common/phase1/config/uncertainty-plan.json \
       --binary ../common/phase1/src/followup_bench \
-      --pmu-stat ../pmu/pmu_stat --cpu 3 \
+      --pmu-stat ../pmu/pmu_stat --cpu 29 \
       --events cycles,instructions,L1D-read-access,L1D-read-miss,cache-misses \
       --events-per-run 2 \
-      --out ../../data_raw/upgrade/pmu/associativity_correlation_l1
+      --out ../../data_raw/artemisia/pmu/associativity_correlation_l1
 
 python3 associativity_correlation.py \
-      --host upgrade --level l2 --period 65536 --ways 4 \
+      --host artemisia --level l2 --period 106496 --ways 16 \
       --binary ../common/phase1/src/followup_bench \
-      --pmu-stat ../pmu/pmu_stat --cpu 3 \
+      --pmu-stat ../pmu/pmu_stat --cpu 29 \
       --events cycles,instructions,LL-read-access,LL-read-miss,cache-misses \
       --events-per-run 2 \
-      --out ../../data_raw/upgrade/pmu/associativity_correlation_l2
+      --out ../../data_raw/artemisia/pmu/associativity_correlation_l2
 
 echo "Bash script finished."
